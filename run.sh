568e60e4ae9f7da6c54362a3fd1df277de007003
sm01 <sm01@vws30.(none)>
2017-03-15 11:47:24 +0900

    add new script file run.sh is ture script

---
+++ run.sh
@@ -0,0 +1,19 @@
+dir=~/dotfiles
+older=~/dotfiles_old
+files="vimrc"
+
+# Create dotfiles_old in homedir
+
+echo "Creating $older for backup of any existing dotfiles in ~"
+mkdir $older
+echo "...done"
+
+# move any existig dotfiles in homedir to dotfiles_old.
+# and then create symlink
+for file in $files 
+do
+    echo "Moving any existing dotfiles from ~ to $older"
+    mv ~/.$file ~/dotfiles_old/
+    echo "Creating symlink to $files in home directory"
+    ln -snf $dir/$file ~/.$file
+done
